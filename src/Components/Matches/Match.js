c8ebaa22aad1da47ce6c6acbf6f4440dc13c93bc
Bia <felipe.silvad@outlook.com>
2023-07-20 15:57:53 -0300

    scroll up

--- src/Components/Matches/Match.js
+++ src/Components/Matches/Match.js
@@ -14,6 +14,7 @@ function MatchComponent() {
   const [match, setMatch] = useState([])
 
   useEffect(() => {
+    window.scrollTo(0, 0)
     onSnapshot(doc(db, "/sets/", id), (doc) => {
       setMatch(doc.data());
     });
